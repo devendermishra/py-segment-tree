dfd4e8cbe0bba7817511dc8fd6f583c0e3d21164
devendermishra <mishra.mis@gmail.com>
2014-12-21 21:41:52 +0530

    Implement Interval sum using SegmentTree class

---
+++ sum-2.py
@@ -0,0 +1,53 @@
+
+import seg_tree
+
+def use_val(left, right):
+  return left + right
+
+def update_val(result, old_val, new_val):
+  result += (new_val-old_val)
+
+
+
+def main():
+  #This will be an interactive application
+  print "Enter the number of elements in the array."
+  n = int(raw_input())
+  i = 0
+  print "Enter the elements separated by spaces. Press Enter after each input."
+  arr = []
+  while i<n:
+    num = int(raw_input())
+    arr.append(num)
+    i +=1
+
+  print "Now, entering into loop"
+  print "To exit, press q/Q"
+  print "To query, press G <start range> <end range>"
+  print "To update, press U <index> <new value>"
+  
+  segtree = seg_tree.SegmentTree(use_val, update_val, 0)
+  segtree.construct(arr)
+  
+  exit = False
+  while not exit:
+    print "Enter the operation and index"
+    inp = raw_input()
+    if str(inp) == 'q' or str(inp)=='Q':
+      exit = True
+    if inp[0]=='G':
+      ptw = map(int, inp[2:].split())
+      start = ptw[0]
+      end = ptw[1]
+      ans = segtree.getresult(start, end)
+      print ans
+      print "Min in ["+str(start)+","+str(end)+"] is "+str(ans)
+    elif inp[0]=='U':
+      ptw = map(int, inp[2:].split())
+      index = ptw[0]
+      new_val = ptw[1]
+      segtree.update(index, new_val)
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
